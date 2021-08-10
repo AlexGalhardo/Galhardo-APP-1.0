408d802b36d1ed90cebc52e3a8657ec6a4f08c91
alexgalhardo <aleexgvieira@gmail.com>
2021-08-10 20:30:35 -0300

    update scroll bar blog

--- public/scripts/scroll_bar.js
+++ public/scripts/scroll_bar.js
@@ -1,7 +1,7 @@
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {scrollBar()};
 
-function myFunction() {
+function scrollBar() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
