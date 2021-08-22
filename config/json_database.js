7ba4b35b4c2060cf5c3b167b76cca8aa3fa7fe25
alexgalhardo <aleexgvieira@gmail.com>
2021-08-22 08:10:09 -0300

    first commit branch api-docs

--- config/json_database.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra')
-
-const JSON_DATABASE_FILE = "./database.json";
-const database = JSON.parse(fs.readFileSync(JSON_DATABASE_FILE));
-
-module.exports = {
-	JSON_DATABASE_FILE,
-	database
-}
