5025b47d19dd1c2789d3863be33f300028e59c39
alex <aleexgvieira@gmail.com>
2021-09-26 20:19:40 -0300

    update models mysql

--- models/MYSQL/Games.js
+++ models/MYSQL/Games.js
@@ -8,55 +8,83 @@
  */
 
 
+// HELPERS
 const DateTime = require('../../helpers/DateTime');
+
+// CONFIG
 const MYSQL = require('../../config/mysql');
 
 
 class Games {
 
+    static async selectAll()  {
+        try {
+            let stmt = `SELECT *
+                        FROM games`;
 
-    static async getAll(){
-        let stmt = "SELECT * FROM games";
+            const [ rows ] = await MYSQL.execute(stmt)
 
-        const [ rows ] = await MYSQL.execute(stmt);
+            console.log('selectAll: ', rows ? rows : null)
 
-        console.log(rows)
+            // return rows ? rows : null
 
-        return rows ? rows : false
+        } catch (error) {
+            throw new Error(error);
+        };
     }
 
 
-    static async getTotal(){
-        let stmt = "SELECT COUNT(id) as totalGames FROM games";
+    static async getTotal()  {
+        try {
+            let stmt = "SELECT COUNT(id) as totalGames FROM games";
 
-        const [ rows ] = await MYSQL.execute(stmt);
+            const [ rows ] = await MYSQL.execute(stmt);
 
-        return rows ? rows[0].totalGames : false
+            console.log('getTotal: ', rows ? rows[0].totalGames : false)
+
+            // return rows ? rows[0].totalGames : false
+        } catch (error) {
+            throw new Error(error);
+        };
     }
 
 
-    static async getRandom(){
-        let stmt = `SELECT * FROM games ORDER BY RAND() LIMIT 1;`;
+    static async getRandom()  {
+        try {
+            let stmt = `SELECT * FROM games ORDER BY RAND() LIMIT 1;`;
 
-        const [ rows ] = await MYSQL.execute(stmt);
+            const [ rows ] = await MYSQL.execute(stmt);
 
-        return rows ? rows : false
+            console.log('getRandom: ', rows ? rows : false)
+
+            // return rows ? rows : false
+        } catch (error) {
+            throw new Error(error);
+        };
     }
 
 
-    static async getByID(game_id){
-        let stmt = `SELECT * FROM games WHERE id = ${game_id}`;
+    static async selectByID(game_id) {
+        try {
+            let stmt = `SELECT * FROM games WHERE id = ${game_id}`;
 
-        const [ rows ] = await MYSQL.execute(stmt);
+            const [ rows ] = await MYSQL.execute(stmt);
 
-        return rows ? rows : false
+            console.log('selectByID: ', rows ? rows : false)
+
+            // return rows ? rows : false
+        } catch (error) {
+            throw new Error(error);
+        }
     }
 
 
-    static async create(gameObject){
-        let stmt = `INSERT INTO games
+    static async create(gameObject) {
+        try {
+            let stmt = `INSERT INTO games
                                 (title,
                                 year_release,
+                                price,
                                 resume,
                                 image,
                                 igdb_link,
@@ -67,34 +95,42 @@ class Games {
                                 amazon_link,
                                 created_at,
                                 updated_at)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
-        let data = [
-            gameObject.title,
-            gameObject.year_release,
-            gameObject.resume,
-            gameObject.image,
-            gameObject.igdb_link,
-            gameObject.igdb_rating,
-            gameObject.platforms,
-            gameObject.developer,
-            gameObject.genres,
-            gameObject.amazon_link,
-            DateTime.getNow(),
-            DateTime.getNow()
-        ];
+            let data = [
+                gameObject.title,
+                gameObject.year_release,
+                gameObject.price,
+                gameObject.resume,
+                gameObject.image,
+                gameObject.igdb_link,
+                gameObject.igdb_rating,
+                gameObject.platforms,
+                gameObject.developer,
+                gameObject.genres,
+                gameObject.amazon_link,
+                DateTime.getNow(),
+                DateTime.getNow()
+            ];
 
-        const [ rows ] = await MYSQL.execute(stmt, data);
+            const [ rows ] = await MYSQL.execute(stmt, data);
 
-        return rows ? rows : false
+            rows.affectedRows ?  console.log(`GAME: ${gameObject.title} CREATED!`) : console.log(`GAME: ${gameObject.title} NOT CREATED!`)
+
+            // return rows ? rows : false
+        } catch (error) {
+            throw new Error(error);
+        }
     }
 
 
-    static async update(gameObject){
-        let stmt = `UPDATE games
+    static async update(gameObject) {
+        try {
+            let stmt = `UPDATE games
                     SET
                         title = '${gameObject.title}',
                         year_release = '${gameObject.year_release}',
+                        price = '${gameObject.price}',
                         resume = '${gameObject.resume}',
                         image = '${gameObject.image}',
                         igdb_link = '${gameObject.igdb_link}',
@@ -107,18 +143,43 @@ class Games {
                     WHERE
                         id = '${gameObject.id}'`;
 
-        const [ rows ] = await MYSQL.execute(stmt);
+            const [ rows ] = await MYSQL.execute(stmt);
 
-        return rows ? rows : false
+            rows.affectedRows ? console.log(`GAME ID: ${gameObject.id} UPDATED!`) : console.log(`GAME ID: ${gameObject.id} NOT UPDATED!`)
+
+            // return rows ? rows : false
+        } catch (error) {
+            throw new Error(error);
+        }
     }
 
 
     static async delete(game_id){
-        let stmt = `DELETE FROM games WHERE id = '${game_id}'`;
+        try {
+            let stmt = `DELETE FROM games WHERE id = '${game_id}'`;
 
-        const [ rows ] = await MYSQL.execute(stmt);
+            const [ rows ] = await MYSQL.execute(stmt);
 
-        return rows.affectedRows ? `GAME ID ${game_id} DELETED!` : `GAME ID ${game_id} NOT Deleted!`
+            console.log('delete: ', rows.affectedRows ? `GAME_ID: ${game_id} DELETED!` : `GAME_ID ${game_id} NOT Deleted!`)
+
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
+
+
+    static async searchTitle(game_title){
+        try {
+            let stmt = `SELECT * FROM games WHERE title LIKE '%${game_title}%'`;
+
+            const [ rows ] = await MYSQL.execute(stmt);
+
+            console.log('searchTitle: ', rows ? rows : false)
+
+            // return rows ? rows : false
+        } catch (error) {
+            throw new Error(error)
+        }
     }
 }
 
--- models/MYSQL/dump_mysql.sql
+++ models/MYSQL/dump_mysql.sql
@@ -3,18 +3,10 @@
 SET NAMES utf8;
 SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP DATABASE IF EXISTS `galhardoapp`;
-CREATE DATABASE `galhardoapp` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `galhardoapp`
 USE `galhardoapp`;
-
--- Adminer 4.8.1 MySQL 8.0.26 dump
-
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP TABLE IF EXISTS `blog`;
 CREATE TABLE `blog` (
@@ -27,51 +19,50 @@ CREATE TABLE `blog` (
   `body` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `created_at` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `updated_at` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `title` (`title`,`slug`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
-INSERT INTO `blog` (`id`, `title`, `slug`, `resume`, `image`, `category`, `body`, `created_at`, `updated_at`) VALUES
-(1, 'patched blog title', 'patched-blog-title', 'patched blog resume',  'https://picsum.photos/id/20/230/230',  'patched blog category',  'patched blog body',  '02/09/2021 18:59:23',  '02/09/2021 19:00:59');
 
 DROP TABLE IF EXISTS `blog_comments`;
 CREATE TABLE `blog_comments` (
   `comment_id` int NOT NULL AUTO_INCREMENT,
+  `blog_id` int NOT NULL,
   `user_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `user_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `user_avatar` varchar(128) NOT NULL,
   `comment` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `created_at` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`comment_id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `blog_comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET DEFAULT
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  KEY `blog_id` (`blog_id`),
+  CONSTRAINT `blog_comments_ibfk_2` FOREIGN KEY (`blog_id`) REFERENCES `blog` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `blog_comments_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
 
 
 DROP TABLE IF EXISTS `books`;
 CREATE TABLE `books` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `year_release` int NOT NULL,
+  `year_release` int unsigned NOT NULL,
+  `price` int unsigned NOT NULL,
   `image` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `amazon_link` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `amazon_link` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `resume` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `pages` int NOT NULL,
+  `pages` int unsigned NOT NULL,
   `genres` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `author` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `created_at` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `updated_at` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb3;
 
-INSERT INTO `books` (`id`, `title`, `year_release`, `image`, `amazon_link`, `resume`, `pages`, `genres`, `author`, `created_at`, `updated_at`) VALUES
-(2, 'HARRY POTTER E A PEDRA FILOSOFAL', 2020, 'https://images-na.ssl-images-amazon.com/images/I/51C5FSmGlqL._SX331_BO1,204,203,200_.jpg', 'https://www.amazon.com.br/dp/8532531768/ref=s9_acsd_al_bw_c2_x_1_t?pf_rd_m=A3RN7G7QC5MWSZ&pf_rd_s=merchandised-search-4&pf_rd_r=94401YP4EFQBKS2C6QK8&pf_rd_t=101&pf_rd_p=bed818b6-7184-4c08-8637-f6cb1f410f34&pf_rd_i=13348890011',  'Há 20 anos a magia aterrissou no Brasil com a chegada de Harry Potter e a Pedra Filosofal. Para comemorar este aniversário tão especial de um dos livros mais vendidos da história, a Rocco lançou esta coleção de luxo em edição limitada com os sete livros em capa dura, com ilustrações de Brian Selznick (vencedor da Medalha Caldecott) e Mary GrandPré. Uma novidade que irá encantar novos leitores e fãs que acompanham a série por todos esses anos',  256,  'Fiction',  'J.K. Rowling', '02/09/2021 18:35:07',  '02/09/2021 18:35:07'),
-(3, 'HARRY POTTER E A PEDRA FILOSOFAL', 2020, 'https://images-na.ssl-images-amazon.com/images/I/51C5FSmGlqL._SX331_BO1,204,203,200_.jpg', 'https://www.amazon.com.br/dp/8532531768/ref=s9_acsd_al_bw_c2_x_1_t?pf_rd_m=A3RN7G7QC5MWSZ&pf_rd_s=merchandised-search-4&pf_rd_r=94401YP4EFQBKS2C6QK8&pf_rd_t=101&pf_rd_p=bed818b6-7184-4c08-8637-f6cb1f410f34&pf_rd_i=13348890011',  'Há 20 anos a magia aterrissou no Brasil com a chegada de Harry Potter e a Pedra Filosofal. Para comemorar este aniversário tão especial de um dos livros mais vendidos da história, a Rocco lançou esta coleção de luxo em edição limitada com os sete livros em capa dura, com ilustrações de Brian Selznick (vencedor da Medalha Caldecott) e Mary GrandPré. Uma novidade que irá encantar novos leitores e fãs que acompanham a série por todos esses anos',  256,  'Fiction',  'J.K. Rowling', '02/09/2021 18:38:34',  '02/09/2021 18:38:34');
 
 DROP TABLE IF EXISTS `games`;
 CREATE TABLE `games` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `year_release` int NOT NULL,
+  `price` int NOT NULL,
   `resume` varchar(512) NOT NULL,
   `image` varchar(256) NOT NULL,
   `igdb_link` varchar(128) NOT NULL,
@@ -83,54 +74,109 @@ CREATE TABLE `games` (
   `created_at` varchar(32) NOT NULL,
   `updated_at` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb3;
+
+
+DROP TABLE IF EXISTS `pagarme_shop_transactions`;
+CREATE TABLE `pagarme_shop_transactions` (
+  `transaction_id` int unsigned NOT NULL,
+  `total_amount` int unsigned NOT NULL,
+  `payment_card_id` varchar(64) NOT NULL,
+  `payment_card_brand` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `payment_card_first_digits` varchar(6) NOT NULL,
+  `payment_card_last_digits` varchar(6) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `payment_card_expiration_date` varchar(4) NOT NULL,
+  `currency` varchar(3) NOT NULL,
+  `status` varchar(8) NOT NULL,
+  `products_amount` int unsigned NOT NULL,
+  `products` text NOT NULL,
+  `customer_id` varchar(64) NOT NULL,
+  `customer_pagarme_id` int unsigned NOT NULL,
+  `customer_email` varchar(32) NOT NULL,
+  `customer_phone` varchar(32) NOT NULL,
+  `customer_name` varchar(32) NOT NULL,
+  `shipping_zipcode` varchar(32) NOT NULL,
+  `shipping_street` varchar(32) NOT NULL,
+  `shipping_street_number` int unsigned NOT NULL,
+  `shipping_neighborhood` varchar(32) NOT NULL,
+  `shipping_city` varchar(32) NOT NULL,
+  `shipping_state` varchar(12) NOT NULL,
+  `shipping_country` varchar(32) NOT NULL,
+  `shipping_carrier` varchar(32) NOT NULL,
+  `shipping_fee` int unsigned NOT NULL,
+  `shipping_deadline` int unsigned NOT NULL,
+  `created_at` varchar(32) NOT NULL,
+  UNIQUE KEY `transaction_id` (`transaction_id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `pagarme_shop_transactions_ibfk_3` FOREIGN KEY (`customer_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
-INSERT INTO `games` (`id`, `title`, `year_release`, `resume`, `image`, `igdb_link`, `igdb_rating`, `platforms`, `developer`, `genres`, `amazon_link`, `created_at`, `updated_at`) VALUES
-(1, 'Hades',  2020, 'A rogue-lite hack and slash dungeon crawler in which Zagreus, son of Hades the Greek god of the dead, attempts to escape his home and his oppressive father by fighting the souls of the dead through the various layers of the ever-shifting underworld, while getting to know and forging relationships with its inhabitants.',  'https://images.igdb.com/igdb/image/upload/t_cover_big/co39vc.jpg', 'https://www.igdb.com/games/hades--1',  94, 'PC, XboxOne Series S|X, PS4, PS5', 'Supergiant Games', 'Adventure, Hack and slash/Beat \'em up, Indie, Role-playing (RPG)',  'amazon link',  '02/09/2021 19:14:51',  '02/09/2021 19:14:51'),
-(2, 'Hades',  2020, 'A rogue-lite hack and slash dungeon crawler in which Zagreus, son of Hades the Greek god of the dead, attempts to escape his home and his oppressive father by fighting the souls of the dead through the various layers of the ever-shifting underworld, while getting to know and forging relationships with its inhabitants.',  'https://images.igdb.com/igdb/image/upload/t_cover_big/co39vc.jpg', 'https://www.igdb.com/games/hades--1',  94, 'PC, XboxOne Series S|X, PS4, PS5', 'Supergiant Games', 'Adventure, Hack and slash/Beat \'em up, Indie, Role-playing (RPG)',  'amazon link',  '02/09/2021 19:28:07',  '02/09/2021 19:28:07'),
-(3, 'Hades',  2020, 'A rogue-lite hack and slash dungeon crawler in which Zagreus, son of Hades the Greek god of the dead, attempts to escape his home and his oppressive father by fighting the souls of the dead through the various layers of the ever-shifting underworld, while getting to know and forging relationships with its inhabitants.',  'https://images.igdb.com/igdb/image/upload/t_cover_big/co39vc.jpg', 'https://www.igdb.com/games/hades--1',  94, 'PC, XboxOne Series S|X, PS4, PS5', 'Supergiant Games', 'Adventure, Hack and slash/Beat \'em up, Indie, Role-playing (RPG)',  'amazon link',  '02/09/2021 19:28:08',  '02/09/2021 19:28:08');
+
+DROP TABLE IF EXISTS `pagarme_subs_transactions`;
+CREATE TABLE `pagarme_subs_transactions` (
+  `transaction_id` int unsigned NOT NULL,
+  `status` varchar(8) NOT NULL,
+  `payment_card_id` varchar(32) NOT NULL,
+  `payment_card_brand` varchar(6) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `payment_card_first_digits` varchar(6) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `payment_card_last_digits` varchar(6) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `payment_card_expiration_date` varchar(12) NOT NULL,
+  `payment_currency` varchar(6) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `plan_id` varchar(12) NOT NULL,
+  `plan_name` varchar(18) NOT NULL,
+  `plan_amount` int NOT NULL,
+  `plan_current_period_start` varchar(32) NOT NULL,
+  `plan_current_period_end` varchar(32) NOT NULL,
+  `customer_id` varchar(48) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `customer_pagarme_id` varchar(32) NOT NULL,
+  `customer_name` varchar(32) NOT NULL,
+  `customer_email` varchar(64) NOT NULL,
+  `customer_phone` varchar(18) NOT NULL,
+  `created_at` varchar(32) NOT NULL,
+  UNIQUE KEY `transaction_id` (`transaction_id`),
+  KEY `customer_id` (`customer_id`),
+  CONSTRAINT `pagarme_subs_transactions_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` varchar(128) NOT NULL,
   `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `email` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `confirmed_email` binary(1) NOT NULL,
-  `confirm_email_token` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `confirmed_email` tinyint DEFAULT NULL,
+  `confirm_email_token` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `password` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `reset_password_token` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `admin` binary(1) NOT NULL,
-  `avatar` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `document` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `phone` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `birth_date` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `google_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `github_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `facebook_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `address_zipcode` varchar(24) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `address_street` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `address_street_number` int NOT NULL,
-  `address_neighborhood` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `address_city` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `address_state` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `address_country` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `stripe_customer_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `stripe_card_token_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `stripe_card_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `stripe_card_last_4_digits` int NOT NULL,
-  `stripe_card_exp_month` int NOT NULL,
-  `stripe_card_exp_year` int NOT NULL,
-  `stripe_currently_subscription_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `stripe_currently_subscription_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `stripe_subscription_start` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `stripe_subscription_end` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `stripe_subscription_automatically_renew` binary(1) NOT NULL,
-  `created_at` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `updated_at` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `reset_password_token` varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `admin` tinyint NOT NULL,
+  `avatar` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `document` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `phone_country` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `phone_ddd` varchar(3) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `phone_number` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `birthday` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `google_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `github_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `facebook_id` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `address_zipcode` varchar(24) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `address_street` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `address_street_number` int DEFAULT NULL,
+  `address_neighborhood` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `address_city` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `address_state` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `address_country` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `pagarme_customer_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `pagarme_card_id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `pagarme_card_brand` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `pagarme_card_first_digits` varchar(6) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `pagarme_card_last_digits` int DEFAULT NULL,
+  `pagarme_card_expiration_date` int DEFAULT NULL,
+  `pagarme_currently_subscription_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `pagarme_currently_subscription_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `pagarme_subscription_start` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `pagarme_subscription_end` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `pagarme_subscription_cancel_at_period_end` tinyint(1) DEFAULT NULL,
+  `created_at` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `updated_at` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
-INSERT INTO `users` (`id`, `name`, `email`, `confirmed_email`, `confirm_email_token`, `password`, `reset_password_token`, `admin`, `avatar`, `document`, `phone`, `birth_date`, `google_id`, `github_id`, `facebook_id`, `address_zipcode`, `address_street`, `address_street_number`, `address_neighborhood`, `address_city`, `address_state`, `address_country`, `stripe_customer_id`, `stripe_card_token_id`, `stripe_card_id`, `stripe_card_last_4_digits`, `stripe_card_exp_month`, `stripe_card_exp_year`, `stripe_currently_subscription_id`, `stripe_currently_subscription_name`, `stripe_subscription_start`, `stripe_subscription_end`, `stripe_subscription_automatically_renew`, `created_at`, `updated_at`) VALUES
-('7f9c1c22-98f6-4472-a031-d027b7196ba9',  'ADMIN Alex', 'admin@gmail.com',  UNHEX('01'),  'null', '$2b$12$qmFqKwOkAfdQLy04VBtOLObd6FUU7H2P3XKa1f95JbKlLgjyOy4kG', 'null', UNHEX('01'),  'admin_avatar.png', '44557777777',  '18999999999',  '2021-08-05', '', '', '', '17280000', 'Avenida Paulista', 42, 'Bairro Chique',  'São Paulo',  'SP', 'Brazil', 'cus_K8y63RgFsRst7l', 'tok_1JV0ZuHoneB4Zvrp8l5HTFNt', 'card_1JV0ZtHoneB4Zvrp0ZVmggZ3',  4242, 8,  22, 'sub_K9dTRyfii6mNKt', 'PRO',  '02/09/2021 14:59:32',  '02/10/2021 14:59:32',  UNHEX('00'),  '10/08/2021 02:25:35',  '15/08/2021 06:25:35');
-
--- 2021-09-02 22:39:31
